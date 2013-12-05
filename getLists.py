78ffd78b47d294312c022170a87e31e651532026
Yatish <yatishby@gmail.com>
2013-12-06 01:04:41 +0800

    Connected all view and search buttons to output. Yet to implement paging

--- getLists.py
+++ getLists.py
@@ -1,74 +1,147 @@
 #!usr/bin/python
 import connectDb
 
-def getCashierList():
+def getCashierList(option, valueSearch = None):
 	count = None
 	rowCashier = None
 	conn, cur = connectDb.connectToDatabase()
-	cur.execute("SELECT count(*) FROM cashier")
+	if option == 0:
+		countQuery = "SELECT count(*) FROM cashier"
+		resultQuery = "SELECT id FROM cashier"
+	elif option == 2:
+		countQuery = "SELECT count(*) FROM cashier WHERE id = %d" % valueSearch
+		resultQuery = "SELECT id FROM cashier WHERE id = %d" % valueSearch
+	cur.execute(countQuery)
 	count = cur.fetchone()
 	count = count[0]
-	cur.execute("SELECT id FROM cashier")
+	cur.execute(resultQuery)
 	rowCashier = cur.fetchall()
 	connectDb.closeDatabaseConnection(conn, cur)
 	return count, rowCashier
 
-def getPDUList():
+
+
+def getPDUList(option, valueSearch = None):
 	count = None
 	rowPDUs = None
 	conn, cur = connectDb.connectToDatabase()
-	cur.execute("SELECT count(*) FROM pdumap")
+	if option == 0:
+		countQuery = "SELECT count(*) FROM pdumap"
+		resultQuery = "SELECT * FROM pdumap"
+	elif option == 1:
+		countQuery = "SELECT count(*) FROM pdumap WHERE barcode = %d" % valueSearch
+		resultQuery = "SELECT * FROM pdumap WHERE barcode = %d" % valueSearch
+	elif option == 2:
+		countQuery = "SELECT count(*) FROM pdumap WHERE id = %d" % valueSearch
+		resultQuery = "SELECT * FROM pdumap WHERE id = %d" % valueSearch
+	cur.execute(countQuery)
 	count = cur.fetchone()
 	count = count[0]
-	cur.execute("SELECT * FROM pdumap")
+	cur.execute(resultQuery)
 	rowPDUs = cur.fetchall()
 	connectDb.closeDatabaseConnection(conn, cur)
 	return count, rowPDUs
 
-def getTranList():
+
+
+def getTranList(option, valueSearch = None):
 	count = None
 	rowTran = None
 	conn, cur = connectDb.connectToDatabase()
-	cur.execute("SELECT count(*) FROM transaction")
+	if option == 0:
+		countQuery = "SELECT count(*) FROM transaction"
+		resultQuery = "SELECT * FROM transaction"
+	elif option == 1:
+		countQuery = "SELECT count(*) FROM transactiondetails WHERE transactionid = %d" % valueSearch
+		resultQuery = "SELECT * FROM transactiondetails WHERE transactionid = %d" % valueSearch
+	elif option == 2:
+		countQuery = "SELECT count(*) FROM transaction t WHERE t.date = '%s'" % valueSearch
+		resultQuery = "SELECT * FROM transaction t WHERE t.date = '%s'" % valueSearch
+	cur.execute(countQuery)
 	count = cur.fetchone()
 	count = count[0]
-	cur.execute("SELECT * FROM transaction")
+	cur.execute(resultQuery)
 	rowTran = cur.fetchall()
 	connectDb.closeDatabaseConnection(conn, cur)
 	return count, rowTran
 
-def getProductList():
+
+
+def getProductList(option, valueSearch = None):
 	count = None
 	rowProducts = None
 	conn, cur = connectDb.connectToDatabase()
-	cur.execute("SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND ISNULL(batch.expiry)")
+	
+	if option == 0:
+		countQuery = "SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND ISNULL(batch.expiry)"
+		resultQuery = "SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND ISNULL(b.expiry)"
+	elif option == 1:
+		countQuery = "SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND ISNULL(batch.expiry) AND product.barcode= %d" % valueSearch
+		resultQuery = "SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND ISNULL(b.expiry) AND p.barcode= %d" % valueSearch
+	elif option == 2:
+		valueSearch = '%' + valueSearch + '%'
+		countQuery = "SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND ISNULL(batch.expiry) AND product.name LIKE '%s'" % valueSearch
+		resultQuery = "SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND ISNULL(b.expiry) AND p.name LIKE '%s'" % valueSearch
+	
+	cur.execute(countQuery)
 	count = cur.fetchone()
 	count = count[0]
-	cur.execute("SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND ISNULL(b.expiry)")
+	cur.execute(resultQuery)
 	rowProducts = cur.fetchall()
 	connectDb.closeDatabaseConnection(conn, cur)
 	return count, rowProducts
 
-def getPerishableList():
+
+
+def getPerishableList(option, valueSearch = None):
 	count = None
 	rowProducts = None
 	conn, cur = connectDb.connectToDatabase()
-	cur.execute("SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND NOT ISNULL(batch.expiry)")
+
+	if option == 0:
+		countQuery = "SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND NOT ISNULL(batch.expiry)"
+		resultQuery = "SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND NOT ISNULL(b.expiry)"
+	elif option == 1:
+		countQuery = "SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND NOT ISNULL(batch.expiry) AND product.barcode= %d" % valueSearch
+		resultQuery = "SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND NOT ISNULL(b.expiry) AND p.barcode= %d" % valueSearch
+	elif option == 2:
+		valueSearch = '%' + valueSearch + '%'
+		countQuery = "SELECT count(distinct product.barcode) FROM product, batch WHERE product.barcode=batch.barcode AND product.active=1 AND NOT ISNULL(batch.expiry) AND product.name LIKE '%s'" % valueSearch
+		resultQuery = "SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND NOT ISNULL(b.expiry) AND p.name LIKE '%s'" % valueSearch
+
+	cur.execute(countQuery)
 	count = cur.fetchone()
 	count = count[0]
-	cur.execute("SELECT distinct p.barcode, p.name, p.category, p.manufacturer, p.cost, p.stocklevel FROM product p, batch b WHERE p.barcode=b.barcode AND p.active=1 AND NOT ISNULL(b.expiry)")
+	cur.execute(resultQuery)
 	rowProducts = cur.fetchall()
 	connectDb.closeDatabaseConnection(conn, cur)
 	return count, rowProducts
 
-def getPromoList():
+
+
+def getPromoList(option, valueSearch = None):
 	count = None
 	rowPromo = None
 	conn, cur = connectDb.connectToDatabase()
-	cur.execute("SELECT count(distinct promotion.promoid) FROM product, promotion WHERE product.barcode=promotion.barcode AND product.active=1 AND (ISNULL(promotion.expiry) OR promotion.expiry>CURDATE())")
+
+	if option == 0:
+		countQuery = "SELECT count(distinct promotion.promoid) FROM product, promotion WHERE product.barcode=promotion.barcode AND product.active=1 AND (ISNULL(promotion.expiry) OR promotion.expiry>CURDATE())"
+		resultQuery = "SELECT distinct pr.promoid, pr.barcode, p.name, p.cost, p.stocklevel, pr.value FROM product p, promotion pr WHERE p.barcode=pr.barcode AND p.active=1 AND (ISNULL(pr.expiry) OR pr.expiry>CURDATE())"
+	elif option == 1:
+		countQuery = "SELECT count(distinct promotion.promoid) FROM product, promotion WHERE product.barcode=promotion.barcode AND product.active=1 AND (ISNULL(promotion.expiry) OR promotion.expiry>CURDATE()) AND product.barcode= %d" % valueSearch
+		resultQuery = "SELECT distinct pr.promoid, pr.barcode, p.name, p.cost, p.stocklevel, pr.value FROM product p, promotion pr WHERE p.barcode=pr.barcode AND p.active=1 AND (ISNULL(pr.expiry) OR pr.expiry>CURDATE()) AND p.barcode= %d" % valueSearch
+	elif option == 2:
+		countQuery = "SELECT count(distinct promotion.promoid) FROM product, promotion WHERE product.barcode=promotion.barcode AND product.active=1 AND (ISNULL(promotion.expiry) OR promotion.expiry>CURDATE()) AND promotion.promoid= %d" % valueSearch
+		resultQuery = "SELECT distinct pr.promoid, pr.barcode, p.name, p.cost, p.stocklevel, pr.value FROM product p, promotion pr WHERE p.barcode=pr.barcode AND p.active=1 AND (ISNULL(pr.expiry) OR pr.expiry>CURDATE()) AND pr.promoid= %d" % valueSearch
+	elif option == 3:
+		valueSearch = '%' + valueSearch + '%'
+		countQuery = "SELECT count(distinct promotion.promoid) FROM product, promotion WHERE product.barcode=promotion.barcode AND product.active=1 AND (ISNULL(promotion.expiry) OR promotion.expiry>CURDATE()) AND product.name LIKE '%s'" % valueSearch
+		resultQuery = "SELECT distinct pr.promoid, pr.barcode, p.name, p.cost, p.stocklevel, pr.value FROM product p, promotion pr WHERE p.barcode=pr.barcode AND p.active=1 AND (ISNULL(pr.expiry) OR pr.expiry>CURDATE()) AND p.name LIKE '%s'" % valueSearch
+
+	cur.execute(countQuery)
 	count = cur.fetchone()
 	count = count[0]
-	cur.execute("SELECT distinct pr.promoid, pr.barcode, p.name, p.cost, p.stocklevel, pr.value FROM product p, promotion pr WHERE p.barcode=pr.barcode AND p.active=1 AND (ISNULL(pr.expiry) OR pr.expiry>CURDATE())")
+	cur.execute(resultQuery)
 	rowPromo = cur.fetchall()
 	connectDb.closeDatabaseConnection(conn, cur)
 	return count, rowPromo
--- retailShopHome.py
+++ retailShopHome.py
@@ -11,56 +11,221 @@ from PyQt4 import QtGui, QtCore
 from MainWindow import Ui_MainWindow
 
 
+def isNumber(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.initialiseAllViews()
+
+        #widgetPressed = self.ui.tabWidget.currentIndex()
+        self.ui.viewall_products.clicked.connect(self.viewProducts)
+        self.ui.searchbarcode_products.clicked.connect(self.viewProducts)
+        self.ui.searchname_products.clicked.connect(self.viewProducts)
+
+        self.ui.viewall_perishable.clicked.connect(self.viewPerishables)
+        self.ui.searchbarcode_perishable.clicked.connect(self.viewPerishables)
+        self.ui.searchname_perishable.clicked.connect(self.viewPerishables)
+
+        self.ui.viewall_tran.clicked.connect(self.viewTran)
+        self.ui.searchid_tran.clicked.connect(self.viewTran)
+        self.ui.searchdate_tran.clicked.connect(self.viewTran)
+
+        self.ui.viewall_units.clicked.connect(self.viewUnits)
+        self.ui.searchbarcode_units.clicked.connect(self.viewUnits)
+        self.ui.searchdev_units.clicked.connect(self.viewUnits)
+
+        self.ui.viewall_promo.clicked.connect(self.viewPromo)
+        self.ui.searchbarcode_promo.clicked.connect(self.viewPromo)
+        self.ui.searchname_promo.clicked.connect(self.viewPromo)
+        self.ui.searchpromo_promo.clicked.connect(self.viewPromo)
+
+        self.ui.orderproducts.clicked.connect(self.orderProducts)
+        self.ui.updatestock.clicked.connect(self.updateStockAndProduct)
+
+    def initialiseAllViews(self):
         self.viewProducts()
         self.viewPerishables()
         self.viewTran()
         self.viewUnits()
         self.viewPromo()
 
-        #widgetPressed = self.ui.tabWidget.currentIndex()
-        self.ui.viewall_products.clicked.connect(self.viewProducts)
-        self.ui.viewall_perishable.clicked.connect(self.viewPerishables)
-        self.ui.viewall_tran.clicked.connect(self.viewTran)
-        self.ui.viewall_units.clicked.connect(self.viewUnits)
-        self.ui.viewall_promo.clicked.connect(self.viewPromo)
-
-        self.ui.searchbarcode_products.clicked.connect
-
-        self.ui.orderproducts.clicked.connect(self.orderProducts)
-        self.ui.updatestock.clicked.connect(self.updateStockAndProduct)
-
     def viewProducts(self):
         self.ui.statusBar.showMessage("Loading Data, please wait...")
-        count, rowProduct = getLists.getProductList()
-        self.arrayToTable(rowProduct, self.ui.tableWidget_products, count, count, 6)
+        flag = 1
+        sender = self.sender()
+        if sender is not None:
+            senderEvent = sender.text()
+            if senderEvent == "View All":
+                self.ui.lineEdit_1_products.clear()
+                self.ui.lineEdit_2_products.clear()
+                count, rowProduct = getLists.getProductList(0)
+            elif (senderEvent == "Barcode") & isNumber(self.ui.lineEdit_1_products.text()):
+                self.ui.lineEdit_2_products.clear()
+                barcodeSearch = int(self.ui.lineEdit_1_products.text())
+                count, rowProduct = getLists.getProductList(1, barcodeSearch)
+            elif senderEvent == "Name":
+                self.ui.lineEdit_1_products.clear()
+                nameSearch = str(self.ui.lineEdit_2_products.text())
+                count, rowProduct = getLists.getProductList(2, nameSearch)
+            else:
+                self.ui.lineEdit_1_products.clear()
+                self.ui.lineEdit_2_products.clear()
+                self.ui.statusBar.clearMessage()
+                flag = 0
+        else:
+            count, rowProduct = getLists.getProductList(0)
+        if flag:
+            self.arrayToTable(rowProduct, self.ui.tableWidget_products, count, count, 6)
 
     def viewPerishables(self):
+        flag = 1
         self.ui.statusBar.showMessage("Loading Data, please wait...")
-        count, rowProduct = getLists.getPerishableList()
-        self.arrayToTable(rowProduct, self.ui.tableWidget_perishable, count, count, 6)
+        sender = self.sender()
+        if sender is not None:
+            senderEvent = sender.text()
+            if senderEvent == "View All":
+                self.ui.lineEdit_1_perishable.clear()
+                self.ui.lineEdit_2_perishable.clear()
+                count, rowProduct = getLists.getPerishableList(0)
+            elif (senderEvent == "Barcode") & isNumber(self.ui.lineEdit_1_perishable.text()):
+                self.ui.lineEdit_2_perishable.clear()
+                barcodeSearch = int(self.ui.lineEdit_1_perishable.text())
+                count, rowProduct = getLists.getPerishableList(1, barcodeSearch)
+            elif senderEvent == "Name":
+                self.ui.lineEdit_1_perishable.clear()
+                nameSearch = str(self.ui.lineEdit_2_perishable.text())
+                count, rowProduct = getLists.getPerishableList(2, nameSearch)
+            else:
+                self.ui.lineEdit_1_perishable.clear()
+                self.ui.lineEdit_2_perishable.clear()
+                self.ui.statusBar.clearMessage()
+                flag = 0
+        else:
+            count, rowProduct = getLists.getPerishableList(0)
+        if flag:
+            self.arrayToTable(rowProduct, self.ui.tableWidget_perishable, count, count, 6)
 
     def viewTran(self):
         self.ui.statusBar.showMessage("Loading Data, please wait...")
-        count, rowTran = getLists.getTranList()
-        self.arrayToTable(rowTran, self.ui.tableWidget_tran, count, count, 4)
+        sender = self.sender()
+        if sender is not None:
+            senderEvent = sender.text()
+            if senderEvent == "View All":
+                self.ui.lineEdit_1_tran.clear()
+                self.ui.lineEdit_2_tran.clear()
+                count, rowTran = getLists.getTranList(0)
+                self.arrayToTable(rowTran, self.ui.tableWidget_tran, count, count, 4)
+            elif (senderEvent == "Transaction ID") & isNumber(self.ui.lineEdit_1_tran.text()):
+                self.ui.lineEdit_2_tran.clear()
+                idSearch = int(self.ui.lineEdit_1_tran.text())
+                count, rowTran = getLists.getTranList(1, idSearch)
+                self.arrayToTable(rowTran, self.ui.tableWidget_tran, count, count, 5)
+            elif senderEvent == "Date":
+                self.ui.lineEdit_1_tran.clear()
+                dateSearch = str(self.ui.lineEdit_2_tran.text())
+                year = dateSearch[:4]
+                month = dateSearch[5:7]
+                day = dateSearch[8:]
+                if isNumber(year) & isNumber(month) & isNumber(day):
+                    dateInput = QtCore.QDate(int(year), int(month), int(day))
+                    if dateInput:
+                        count, rowTran = getLists.getTranList(2, dateSearch)
+                        self.arrayToTable(rowTran, self.ui.tableWidget_tran, count, count, 4)
+                    else:
+                        self.ui.lineEdit_1_tran.clear()
+                        self.ui.lineEdit_2_tran.clear()
+                        self.ui.statusBar.clearMessage()
+                else:
+                    self.ui.lineEdit_1_tran.clear()
+                    self.ui.lineEdit_2_tran.clear()
+                    self.ui.statusBar.clearMessage()                
+            else:
+                self.ui.lineEdit_1_tran.clear()
+                self.ui.lineEdit_2_tran.clear()
+                self.ui.statusBar.clearMessage()
+        else:
+            count, rowTran = getLists.getTranList(0)
+            self.arrayToTable(rowTran, self.ui.tableWidget_tran, count, count, 4)
 
     def viewUnits(self):
+        flag = 1
         self.ui.statusBar.showMessage("Loading Data, please wait...")
-        self.ui.tableWidget_cashier.setHorizontalHeaderLabels(QtCore.QString('Cashier Device Unit'))
-    	count, rowCashier = getLists.getCashierList()
-        self.arrayToTable(rowCashier, self.ui.tableWidget_cashier, count, count, 1)
-        count, rowPDUs = getLists.getPDUList()
-        self.arrayToTable(rowPDUs, self.ui.tableWidget_pdu, count, count, 2)
+        sender = self.sender()
+        if sender is not None:
+            senderEvent = sender.text()
+            if senderEvent == "View All":
+                self.ui.lineEdit_1_units.clear()
+                self.ui.lineEdit_2_units.clear()
+                countCashier, rowCashier = getLists.getCashierList(0)
+                countPdu, rowPDUs = getLists.getPDUList(0)
+            elif (senderEvent == "Barcode") & isNumber(self.ui.lineEdit_1_units.text()):
+                self.ui.lineEdit_2_units.clear()
+                barcodeSearch = int(self.ui.lineEdit_1_units.text())
+                countCashier, rowCashier = getLists.getCashierList(0)
+                countPdu, rowPDUs = getLists.getPDUList(1, barcodeSearch)
+            elif (senderEvent == "Device ID") & isNumber(self.ui.lineEdit_2_units.text()):
+                self.ui.lineEdit_1_units.clear()
+                nameSearch = int(self.ui.lineEdit_2_units.text())
+                countCashier, rowCashier = getLists.getCashierList(2, nameSearch)
+                countPdu, rowPDUs = getLists.getPDUList(2, nameSearch)
+            else:
+                self.ui.lineEdit_1_units.clear()
+                self.ui.lineEdit_2_units.clear()
+                self.ui.statusBar.clearMessage()
+                flag = 0
+        else:
+            countCashier, rowCashier = getLists.getCashierList(0)
+            countPdu, rowPDUs = getLists.getPDUList(0)
+        if flag:
+            self.arrayToTable(rowCashier, self.ui.tableWidget_cashier, countCashier, countCashier, 1)
+            self.arrayToTable(rowPDUs, self.ui.tableWidget_pdu, countPdu, countPdu, 2)
 
     def viewPromo(self):
+        flag = 1
         self.ui.statusBar.showMessage("Loading Data, please wait...")
-        count, rowPromo = getLists.getPromoList()
-        self.arrayToTable(rowPromo, self.ui.tableWidget_promo, count, count, 6)
+        sender = self.sender()
+        if sender is not None:
+            senderEvent = sender.text()
+            if senderEvent == "View All":
+                self.ui.lineEdit_1_promo.clear()
+                self.ui.lineEdit_2_promo.clear()
+                self.ui.lineEdit_3_promo.clear()
+                count, rowPromo = getLists.getPromoList(0)
+            elif (senderEvent == "Barcode") & isNumber(self.ui.lineEdit_1_promo.text()):
+                self.ui.lineEdit_2_promo.clear()
+                self.ui.lineEdit_3_promo.clear()
+                barcodeSearch = int(self.ui.lineEdit_1_promo.text())
+                count, rowPromo = getLists.getPromoList(1, barcodeSearch)
+            elif (senderEvent == "Promo ID") & isNumber(self.ui.lineEdit_2_promo.text()):
+                self.ui.lineEdit_1_promo.clear()
+                self.ui.lineEdit_3_promo.clear()
+                idSearch = int(self.ui.lineEdit_2_promo.text())
+                count, rowPromo = getLists.getPromoList(2, idSearch)
+            elif senderEvent == "Name":
+                self.ui.lineEdit_1_promo.clear()
+                self.ui.lineEdit_2_promo.clear()
+                nameSearch = str(self.ui.lineEdit_3_promo.text())
+                count, rowPromo = getLists.getPromoList(3, nameSearch)
+            else:
+                self.ui.lineEdit_1_promo.clear()
+                self.ui.lineEdit_2_promo.clear()
+                self.ui.lineEdit_3_promo.clear()
+                self.ui.statusBar.clearMessage()
+                flag = 0
+        else:
+            count, rowPromo = getLists.getPromoList(0)
+        if flag:
+            self.arrayToTable(rowPromo, self.ui.tableWidget_promo, count, count, 6)
+
 
     def updateStockAndProduct(self):
         self.ui.statusBar.showMessage("Checking for Data, please wait...")
@@ -97,7 +262,7 @@ class MainWindow(QtGui.QMainWindow):
                         cellContent = str(array[row][column])
                     qtable.setItem(row, column, QtGui.QTableWidgetItem(QtCore.QString("%1").arg(cellContent)))
                 else:
-                    qtable.setItem(row, column, QtGui.QTableWidgetItem())
+                    qtable.setItem(row, column, QtGui.QTableWidgetItem(QtCore.QString("NULL")))
         self.ui.statusBar.clearMessage()
 
 
